086bec85ef318fc04feb7b5f47f6e8e97d178acc
ahracho <ahra123@i-commtech.com>
2022-04-15 10:53:50 +0900

    search mapper add

--- src/main/java/com/example/boardapi/board/mapper/board/BoardMapper.java
+++ src/main/java/com/example/boardapi/board/mapper/board/BoardMapper.java
@@ -3,7 +3,11 @@ package com.example.boardapi.board.mapper.board;
 import com.example.boardapi.board.entity.board.Board;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface BoardMapper {
     void insert(Board board);
+
+    List<Board> search(Board board);
 }
